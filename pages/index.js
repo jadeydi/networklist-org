f2fbb12919fec15c2536c10bd050b7cdf0dd04f4
LiYuqing <im.yuqlee@gmail.com>
2022-03-07 12:46:23 +0800

    Add google analytics

--- pages/index.js
+++ pages/index.js
@@ -227,6 +227,15 @@ function Home({ changeTheme, theme }) {
           </div>
         </div>
       </main>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WDVXRXZRDN"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-WDVXRXZRDN');
+      </script>
     </div>
   )
 }
