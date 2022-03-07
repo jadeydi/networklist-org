34c1ca17f43bfa0ed77ea902e3bab5f8c77220ad
LiYuqing <im.yuqlee@gmail.com>
2022-03-07 13:36:57 +0800

    multichain will close soon, disable it first

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ import {
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import Chain from '../components/chain'
-import MultiChain from '../components/multichain'
 import Header from '../components/header'
 
 import SearchIcon from '@material-ui/icons/Search';
@@ -107,7 +106,6 @@ function Home({ changeTheme, theme }) {
 
   const [ layout, setLayout ] = useState('grid')
   const [ search, setSearch ] = useState('')
-  const [ hideMultichain, setHideMultichain ] = useState('1')
   const router = useRouter()
   if (router.query.search) {
     setSearch(router.query.search)
@@ -128,20 +126,6 @@ function Home({ changeTheme, theme }) {
   const addNetwork = () => {
     window.open('https://github.com/ethereum-lists/chains', '_blank')
   }
-
-  const closeMultichain = (perma) => {
-    setHideMultichain('1')
-    localStorage.setItem('chainlist.org-hideMultichain', perma ? '1' : '0')
-  }
-
-  useEffect(() => {
-    const multi = localStorage.getItem('chainlist.org-hideMultichain')
-    if(multi) {
-      setHideMultichain(multi)
-    } else {
-      setHideMultichain('0')
-    }
-  }, [])
 
   return (
     <div className={styles.container}>
@@ -207,10 +191,9 @@ function Home({ changeTheme, theme }) {
               <Header changeTheme={ changeTheme } />
             </div>
             <div className={ classes.cardsContainer }>
-              { hideMultichain === '0' && <MultiChain closeMultichain={ closeMultichain } /> }
               {
                 data && data.filter((chain) => {
-                  if(search === '') {
+                  if (search === '') {
                     return true
                   } else {
                     //filter
